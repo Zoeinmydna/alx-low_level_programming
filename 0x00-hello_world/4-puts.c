203642a5794316d282ccd276418da2d2186518b6
Zoeinmydna <atandaoluwatimileyin1@gmail.com>
2022-10-14 05:46:56 -0700

    cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - print the script
- * Description: "Programming is like building a multilingual puzzle
- * return: 0
+ *main- Serves as the entry point for the program
+ *
+ *Return: By default returns zero if no erros otherwise returns non-zero value.
  */
+
 
 
 int main(void)
