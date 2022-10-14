b03c877434ae9ea7d801594b522512415527e982
Zoeinmydna <atandaoluwatimileyin1@gmail.com>
2022-10-14 05:04:59 -0700

    cisfun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#insert <stdio.h>
+
+/**
+ ** main - print the string in the
+ **
+ ** Description: using the main function
+ ** this program prints "Programming is a like building a multilingual puzzle"
+ ** Return: 0
+ **/
+
+int main(void)
+
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+
+}
