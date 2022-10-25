02564104e48d7d62243e166afa253c96edd6fac3
Zoeinmydna <atandaoluwatimileyin1@gmail.com>
2022-10-24 22:56:36 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen-Finds the lenght of a string
+ * @s: String pointer to the string whose leght is to be found.
+ * Return: retirns the lenght of the string.
+ */
+
+int _strlen(char *s)
+{
+	int p = 0;
+	/**increment up to when the last character is NULL 0*/
+	while (*(s + p) != 0)
+	{
+		p++
+	}
+	return (p);
+}
